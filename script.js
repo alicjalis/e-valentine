7fd91de3804b773a09f454ec35c784cf5020f96b
alicjalis <57289626+alicjalis@users.noreply.github.com>
2025-02-11 03:02:45 +0100

    add custom buttons and onclick yes handling

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const gif = document.getElementById("gif");
 const noButton = document.getElementById("nobutton")
+const yesButton = document.getElementById("yesbutton")
 const firstGifDuration = 5500;
+const valentinesFirstGifDuration = 2000;
 
 // Preload drugiego GIF-a
 const secondGif = new Image();
@@ -9,10 +11,38 @@ secondGif.src = "images/lisek_second.gif";
 const angryGif= new Image();
 angryGif.src = "images/lisek_angry.gif";
 
+const valentinesFirstGif = new Image();
+valentinesFirstGif.src = "images/lisek_valentines_first.gif";
+
+const valentinesSecondGif = new Image();
+valentinesSecondGif.src = "images/lisek_valentines_second.gif";
+
 // Po 5,5 sekundy zmieniamy GIF
 setTimeout(() => {
     gif.src = secondGif.src; // Podmieniamy na już wczytany GIF
+
+    const valentinesText = document.querySelector('.valentine-text');
+    const buttons = document.querySelector('.buttons');
+    
+    valentinesText.style.display = 'block';  // Ustawienie elementu na widoczny
+    buttons.style.display = 'flex';  // Ustawienie elementu na widoczny
+    setTimeout(() => {
+        valentinesText.style.opacity = 1; // Stopniowe pojawienie się
+        buttons.style.opacity = 1; // Stopniowe pojawienie się
+    }, 10); 
 }, firstGifDuration);
+
+yesButton.addEventListener("click", () => {
+    gif.src = valentinesFirstGif.src;
+    noButton.style.display = 'none';  // Ukrywamy przycisk 'No'
+    const valentinesText = document.querySelector('.valentine-text');
+    valentinesText.style.display = 'none';  // Ukrywamy napis
+
+    setTimeout(()=>{
+        gif.src = valentinesSecondGif.src;
+
+    }, valentinesFirstGifDuration)
+});
 
 
 noButton.addEventListener("mouseenter", () => {
@@ -22,3 +52,4 @@ noButton.addEventListener("mouseenter", () => {
 noButton.addEventListener("mouseleave", () => {
     gif.src = secondGif.src;
 })
+
